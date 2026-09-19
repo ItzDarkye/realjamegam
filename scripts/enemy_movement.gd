723006055924976a4c2675c918c7ad59e62e569d
Verdioloo <124391383+Verdioloo@users.noreply.github.com>
2026-09-19 19:16:32 +0200

    round-system
    super-duper-cool round :)

--- scripts/enemy_movement.gd
+++ scripts/enemy_movement.gd
@@ -10,7 +10,9 @@ var health=4
 
 
 func check_if_dead():
-	if health==0:
+	if health==0 or get_parent().enemy_killed==(get_parent().round_number*4):
+		if health==0:
+			get_parent().enemy_killed+=1
 		queue_free()
 
 func knockback():
--- scripts/enemy_movement2.gd
+++ scripts/enemy_movement2.gd
@@ -9,7 +9,9 @@ var instance=preload("res://scenes/arrow.tscn")
 @onready var timer_for_arrows=$Timer
 var timer_is_stopped=true
 func check_if_dead():
-	if health==0:
+	if health==0 or get_parent().enemy_killed==(get_parent().round_number*4):
+		if health==0:
+			get_parent().enemy_killed+=1
 		queue_free()
 
 func knockback():
