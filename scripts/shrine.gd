6d9a6cb852f4bd794336cc40973edb2220a28b1e
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2026-09-20 15:24:28 +0530

    Add shrine that can receive tomato offerings from the player.

---
+++ scripts/shrine.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var offeredTomatoes = 0
+@onready var area = $Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area.body_entered.connect(_on_body_entered)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body : Node2D) -> void:
+	if body is Player:
+		var receivedTomatoes = body.tomato_collector.offer_tomatoes_in_shrine()
+		offeredTomatoes += receivedTomatoes
+		print("Shrine: received ", receivedTomatoes, " tomatoes")
+		print("Shrine: total tomatoes: ", offeredTomatoes)
--- scripts/tomato_collector.gd
+++ scripts/tomato_collector.gd
@@ -23,3 +23,10 @@ func collect_a_tomato() -> void:
 	tomato_collected.emit()
 	print(body, "Collected a Tomato!")
 	print("Number of tomatoes: ", collectedTomatoes)
+
+func offer_tomatoes_in_shrine() -> int:
+	var offeredTomatoes = collectedTomatoes
+	collectedTomatoes = 0
+	print(body, "Offered ", offeredTomatoes, " tomatoes!")
+	print("Number of tomatoes now: ", collectedTomatoes)
+	return offeredTomatoes
